8c4a2bdb388c75dbbfed6f4b863e43a5d61ea0fc
Kostya Kaganov <kkaganov@gmail.com>
2021-04-05 23:16:10 +0300

    Add react routes for Main, SignIn, Favorites and Room components in App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import Main from '../main/main';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {AppRoute, getOfferUrl} from '../../util/route';
+import Main from '../main/main';
+import SignIn from '../sign-in/sign-in';
+import Favorites from '../favorites/favorites';
+import Room from '../room/room';
 
 const App = ({offersCount}) => {
   return (
-    <Main offersCount={offersCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.ROOT}>
+          <Main offersCount={offersCount} />
+        </Route>
+        <Route exact path={AppRoute.LOGIN}>
+          <SignIn />
+        </Route>
+        <Route exact path={AppRoute.FAVORITES}>
+          <Favorites />
+        </Route>
+        <Route exact path={getOfferUrl(`:id`)}>
+          <Room />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
