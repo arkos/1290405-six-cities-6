60f82c5fe524343421564c0e1590e67dd2073a90
Kostya Kaganov <kkaganov@gmail.com>
2021-04-09 00:07:59 +0300

    Configure store using @redux/toolkit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import rootReducer from './store/root-reducer';
+import {configureStore} from '@reduxjs/toolkit';
 import App from "./components/app/app";
 
 const OFFERS_COUNT = 2;
+
+const store = configureStore({
+  reducer: rootReducer
+});
 
 ReactDOM.render(
     <App offersCount={OFFERS_COUNT} />,
