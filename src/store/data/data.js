8071f37956ec824d182ca81af0f96ce8ab79c01c
Kostya Kaganov <kkaganov@gmail.com>
2021-04-09 15:30:52 +0300

    Implement data reducer using RTK createSlice

---
+++ src/store/data/data.js
@@ -0,0 +1,20 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+  offers: []
+};
+
+const data = createSlice({
+  name: `data`,
+  initialState,
+  reducers: {
+    loadOffers: (state, action) => {
+      state.offers = action.payload;
+    }
+  }
+});
+
+const {loadOffers} = data.actions;
+
+export {loadOffers};
+export default data;
