bb65c59f8deb184d5ab9c8b13bb48965837f1770
Kostya Kaganov <kkaganov@gmail.com>
2021-04-10 22:13:12 +0300

    Add middleware to store configuration and pass api argument

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import rootReducer from './store/root-reducer';
 import {configureStore} from '@reduxjs/toolkit';
+import {createAPI} from './services/api';
 import App from "./components/app/app";
 
 const OFFERS_COUNT = 2;
 
+const api = createAPI();
+
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => {
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api
+      }
+    });
+  }
 });
 
 ReactDOM.render(
