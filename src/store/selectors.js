feb9fbc97d68744351143ef70b98dbcdcf92324d
Kostya Kaganov <kkaganov@gmail.com>
2021-04-26 21:03:50 +0300

    Add selectOffersByLimit selector

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,10 +1,16 @@
 import {createSelector} from "reselect";
+import {MAX_OFFERS_COUNT} from "../util/const";
 
-const offerSelector = ({DATA}) => DATA.offers;
+const offersSelector = ({DATA}) => DATA.offers;
 
 const selectAllOffers = createSelector(
-    [offerSelector],
+    [offersSelector],
     (offers) => offers
 );
 
-export {selectAllOffers};
+const selectOffersByLimit = createSelector(
+    [offersSelector],
+    (offers) => offers.slice(0, MAX_OFFERS_COUNT)
+);
+
+export {selectAllOffers, selectOffersByLimit};
