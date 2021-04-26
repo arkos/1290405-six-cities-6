5d2d01b217a9bb73a2faf7e7eb1b14a8b17eebae
Kostya Kaganov <kkaganov@gmail.com>
2021-04-26 22:24:10 +0300

    Add util to adapt offer from server to client

--- src/util/common.js
+++ src/util/common.js
@@ -8,3 +8,26 @@ const roomTypeToNameMap = {
 export const getRoomName = (type) => {
   return roomTypeToNameMap[type] || type;
 };
+
+export const adaptOfferToClient = (offer) => {
+  const adaptedOffer = Object.assign({}, offer, {
+    previewImage: offer.preview_image,
+    isFavorite: offer.is_favorite,
+    isPremium: offer.is_premium,
+    maxAdults: offer.max_adults,
+    host: Object.assign({}, offer.host, {
+      isPro: offer.host.is_pro,
+      avatarUrl: offer.host.avatar_url
+    })
+  });
+
+  delete adaptedOffer.preview_image;
+  delete adaptedOffer.is_favorite;
+  delete adaptedOffer.is_premium;
+  delete adaptedOffer.max_adults;
+  delete adaptedOffer.host.is_pro;
+  delete adaptedOffer.host.avatar_url;
+
+  return adaptedOffer;
+
+};
