c026e62084f0b090651c507eb6d7402eb6877729
Kostya Kaganov <kkaganov@gmail.com>
2021-04-26 22:25:43 +0300

    Render premium label in OfferCard

--- src/components/offer-card/offer-card.jsx
+++ src/components/offer-card/offer-card.jsx
@@ -12,9 +12,9 @@ const OfferCard = ({offer}) => {
 
   return (
     <article className="cities__place-card place-card">
-      <div className="place-card__mark">
-        {true && <span>Premium</span>}
-      </div>
+      {isPremium && <div className="place-card__mark">
+        <span>Premium</span>
+      </div>}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
           <img className="place-card__image" src={apartmentImage} width="260" height="200" alt={title} />
