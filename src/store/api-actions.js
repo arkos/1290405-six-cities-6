711dac0ef32c12368f9178066196f78b84ed174e
Kostya Kaganov <kkaganov@gmail.com>
2021-04-26 22:25:04 +0300

    Change fetchOffers to return adapted offers

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,8 +1,9 @@
 import {ActionType} from '../util/const';
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import {APIRoute} from '../util/route';
+import {adaptOfferToClient} from '../util/common';
 
 export const fetchOffers = createAsyncThunk(ActionType.FETCH_OFFERS, async (_, {extra: api}) => {
   const response = await api.get(APIRoute.OFFERS);
-  return response.data;
+  return response.data.map(adaptOfferToClient);
 });
