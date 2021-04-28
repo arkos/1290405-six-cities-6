fe0543b994258ad36e20aaf5a8ab6e09f767fbf3
Kostya Kaganov <kkaganov@gmail.com>
2021-04-29 00:05:17 +0300

    Add selector to get offers and favorites loading state

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -2,9 +2,13 @@ import {createSelector} from "reselect";
 import {MAX_OFFERS_COUNT} from "../util/const";
 import {selectAllOffers} from '../store/data/data';
 
+const selectOffersStatus = (state) => state.DATA.offersLoadingState;
+
+const selectFavoritesStatus = (state) => state.DATA.favoritesLoadingState;
+
 const selectOffersByLimit = createSelector(
     [selectAllOffers],
     (offers) => offers.slice(0, MAX_OFFERS_COUNT)
 );
 
-export {selectOffersByLimit};
+export {selectOffersByLimit, selectOffersStatus, selectFavoritesStatus};
