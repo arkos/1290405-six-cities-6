05bba5b466a4593f5051303c63f8cde11d0449bd
Kostya Kaganov <kkaganov@gmail.com>
2021-05-01 15:07:00 +0300

    Dispatch redirectToRoute after login

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,7 +1,8 @@
 import {ActionType} from '../util/const';
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import {APIRoute} from '../util/route';
+import {APIRoute, AppRoute} from '../util/route';
 import {adaptOfferToClient, adaptUserToClient} from '../util/common';
+import {redirectToRoute} from './actions';
 
 export const fetchOffers = createAsyncThunk(ActionType.FETCH_OFFERS, async (_, {extra: api}) => {
   const response = await api.get(APIRoute.OFFERS);
@@ -18,9 +19,11 @@ export const checkAuth = createAsyncThunk(ActionType.CHECK_AUTH, async (_, {extr
   return response.data;
 });
 
-export const login = createAsyncThunk(ActionType.LOGIN, async (payload, {extra: api}) => {
+export const login = createAsyncThunk(ActionType.LOGIN, async (payload, {dispatch, _, extra: api}) => {
   const {login: email, password} = payload;
 
   const response = await api.post(APIRoute.LOGIN, {email, password});
+  dispatch(redirectToRoute(AppRoute.ROOT));
+
   return adaptUserToClient(response.data);
 });
