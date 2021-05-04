5df778b1fefe4c2782a57ad08da0af65a78221aa
Kostya Kaganov <kkaganov@gmail.com>
2021-05-04 23:18:47 +0300

    Add selectors to get authorization status and logged in status

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,8 +1,17 @@
 import {createSelector} from "reselect";
-import {MAX_OFFERS_COUNT} from "../util/const";
+import {AuthorizationStatus, MAX_OFFERS_COUNT} from "../util/const";
 import {selectAllOffers} from '../store/data/data';
 
 const selectOffersStatus = (state) => state.DATA.offersLoadingState;
+
+const selectAuthStatus = (state) => state.USER.authorizationStatus;
+
+const selectIsLoggedIn = createSelector(
+    [selectAuthStatus],
+    (authorizationStatus) => authorizationStatus === AuthorizationStatus.AUTH
+);
+
+const selectLoggedInUser = (state) => state.USER.user;
 
 const selectFavoritesStatus = (state) => state.DATA.favoritesLoadingState;
 
@@ -11,4 +20,11 @@ const selectOffersByLimit = createSelector(
     (offers) => offers.slice(0, MAX_OFFERS_COUNT)
 );
 
-export {selectOffersByLimit, selectOffersStatus, selectFavoritesStatus};
+export {
+  selectOffersByLimit,
+  selectIsLoggedIn,
+  selectOffersStatus,
+  selectFavoritesStatus,
+  selectAuthStatus,
+  selectLoggedInUser,
+};
