ecb6173ad2598d7689079dd215b63a561571f786
Kostya Kaganov <kkaganov@gmail.com>
2021-05-04 23:20:01 +0300

    Render sign-in indicator in SignIn and redirect to main page if logged in

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,9 +1,15 @@
 import React, {useState} from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {Redirect} from 'react-router';
 import {login} from '../../store/api-actions';
+import {selectIsLoggedIn} from '../../store/selectors';
+import {AppRoute} from '../../util/route';
+import SignInIndicator from '../sign-in-indicator/sign-in-indicator';
 
 const SignIn = () => {
   const dispatch = useDispatch();
+
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   const [userForm, setUserForm] = useState({
     email: ``,
@@ -26,6 +32,10 @@ const SignIn = () => {
     setUserForm({...userForm, [evt.target.name]: evt.target.value});
   };
 
+  if (isLoggedIn) {
+    return <Redirect to={AppRoute.ROOT}/>;
+  }
+
   return (
     <div className="page page--gray page--login">
       <header className="header">
@@ -36,17 +46,7 @@ const SignIn = () => {
                 <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
               </a>
             </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__login">Sign in</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
+            <SignInIndicator />
           </div>
         </div>
       </header>
