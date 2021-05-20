ff2d6d435f888d635ce959ca458485ef05e0fded
Kostya Kaganov <kkaganov@gmail.com>
2021-05-20 20:22:29 +0300

    Render ReviewsList in Room component

--- src/components/room/room.jsx
+++ src/components/room/room.jsx
@@ -2,9 +2,38 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {AppRoute} from '../../util/route';
 import AddReviewForm from '../add-review-form/add-review-form';
+import ReviewsList from '../reviews-list/reviews-list';
 import SignInIndicator from '../sign-in-indicator/sign-in-indicator';
 
 const Room = () => {
+
+  const reviews = [
+    {
+      "id": 1,
+      "user": {
+        "id": 18,
+        "isPro": true,
+        "name": `Sophie`,
+        "avatarUrl": `https://assets.htmlacademy.ru/intensives/javascript-3/avatar/9.jpg`
+      },
+      "rating": 5,
+      "comment": `The deluxe room was a quite comfortable one with all the adequate facilities. The only thing that made me feel uncomfortable was the rude behavior of an impolite staff at the reception desk.`,
+      "date": `2021-02-23T08:04:28.647Z`
+    },
+    {
+      "id": 2,
+      "user": {
+        "id": 15,
+        "isPro": false,
+        "name": `Kendall`,
+        "avatarUrl": `https://assets.htmlacademy.ru/intensives/javascript-3/avatar/6.jpg`
+      },
+      "rating": 3,
+      "comment": `The house is very good, very happy, hygienic and simple living conditions around it are also very good. I hope to have the opportunity to come back. Thank you.`,
+      "date": `2021-02-23T08:04:28.647Z`
+    }
+  ];
+
   return (
     <div className="page">
       <header className="header">
@@ -140,31 +169,7 @@ const Room = () => {
                 </div>
               </div>
               <section className="property__reviews reviews">
-                <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-                <ul className="reviews__list">
-                  <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
-                      </div>
-                      <span className="reviews__user-name">
-                        Max
-                      </span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span style={{width: `80%`}}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <p className="reviews__text">
-                        A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                    </div>
-                  </li>
-                </ul>
+                <ReviewsList reviews={reviews} />
                 <AddReviewForm />
               </section>
             </div>
