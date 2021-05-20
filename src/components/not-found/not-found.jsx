0aea7ae5cb913d9c80a10f0bc9ed64a67b8bd7a9
Kostya Kaganov <kkaganov@gmail.com>
2021-05-20 20:55:39 +0300

    Fix NotFound component

--- src/components/not-found/not-found.jsx
+++ src/components/not-found/not-found.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../util/route';
 import SignInIndicator from '../sign-in-indicator/sign-in-indicator';
 
 const NotFound = () => {
@@ -8,9 +10,9 @@ const NotFound = () => {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
+              <Link to={AppRoute.ROOT} className="header__logo-link header__logo-link--active">
                 <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              </Link>
             </div>
             <SignInIndicator />
           </div>
