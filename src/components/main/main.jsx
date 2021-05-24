35746660c8842836ba1015032ba32f9c328da015
Kostya Kaganov <kkaganov@gmail.com>
2021-05-24 21:29:39 +0300

    Enable sort menu functionality in Main component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -10,6 +10,14 @@ import Loading from '../loading/loading';
 import {Link} from 'react-router-dom';
 import {AppRoute} from '../../util/route';
 import SignInIndicator from '../sign-in-indicator/sign-in-indicator';
+import SortMenu from '../sort-menu/sort-menu';
+
+const menuItems = [
+  {label: `Popular`, value: `popular`, active: true},
+  {label: `Price: low to high`, value: `asc`},
+  {label: `Price: high to low`, value: `desc`},
+  {label: `Top rated first`, value: `top`},
+];
 
 const Main = () => {
   const offers = useSelector(selectOffersByLimit);
@@ -44,6 +52,11 @@ const Main = () => {
     return <Loading title={`Loading favorites ...`}/>;
   }
 
+  const handleMenuSelect = (currentItem) => {
+    menuItems.forEach((item) => {
+      item.active = item.value === currentItem;
+    });
+  };
 
   return (
     <div className="page page--gray page--main">
@@ -103,21 +116,7 @@ const Main = () => {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">312 places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex="0">
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom">
-                  <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                  <li className="places__option" tabIndex="0">Price: low to high</li>
-                  <li className="places__option" tabIndex="0">Price: high to low</li>
-                  <li className="places__option" tabIndex="0">Top rated first</li>
-                </ul>
-              </form>
+              <SortMenu onMenuClick={() => {}} onMenuSelect={handleMenuSelect} items={menuItems}/>
               <OfferList offers={offers}/>
             </section>
             <div className="cities__right-section">
